72d9b7572e58db19ddf77869ea30aa8390729eea
fResult <styxmaz@gmail.com>
2022-09-01 10:56:38 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,95 @@
+import 'antd/dist/antd.css';
+import { Space, Table, Tag } from 'antd';
+
+import React, { Fragment } from 'react';
+import { ColumnGroupType, ColumnType } from 'antd/lib/table';
+
+type TableDataRow = {
+  key: string | number;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+};
+
+interface ColType<T extends Object> extends Omit<ColumnType<T>, 'key'> {
+  key: keyof T;
+}
+
+const columns: ColType<TableDataRow>[] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <Fragment>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </Fragment>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+const data: TableDataRow[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const App = () => <Table dataSource={data} columns={columns} />;
+
+export default App;
